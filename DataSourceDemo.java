0cbc9df5fead1eabfa6c7747b056c868c378c0b6
Qadir <qadirb3492@gmail.com>
2026-04-03 16:56:50 +0500

    Delete DataSourceDemo.java

--- DataSourceDemo.java
+++
@@ -1,9 +0,0 @@
-class DataSourceDemo{
-    public static void main(String [] args){
-        FileSource file = new FileSource();//creating FileSource
-        System.out.println(file.readLine()); //reading from the file Source
-        
-        KeyboardSource source = new KeyboardSource();//creating a keyboard source
-        System.out.println("Your Entered String is = "+source.readLine());//reading from the keyboard source
-    }
-}
