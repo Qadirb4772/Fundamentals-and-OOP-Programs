73f2a3d549aa490363c47c205fb5d404d99ace73
Qadir <qadirb3492@gmail.com>
2026-05-23 12:52:40 +0500

    Delete LibraryDemo.java

--- LibraryDemo.java
+++
@@ -1,8 +0,0 @@
-public class LibraryDemo{
-  public static void main(String[] args){
-    BorrowableBook b = new BorrowableBook("OOP Guide", "B-19", 14);
-    b.summaryLine();
-    System.out.println(b.availabilityNote());
-    System.out.println(b.dueDayNumber(5));
-  }
-}
